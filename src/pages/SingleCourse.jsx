7ac7e7be9adf08d30a4739c9d9311dd08135572d
Ashkan Khorasani <ashkankhorasanidevelopment@gmail.com>
2024-01-20 16:52:21 +0330

    Start SingleCourse Component

--- src/pages/SingleCourse.jsx
+++ src/pages/SingleCourse.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import styles from './SingleCourse.module.css';
+import {AccessTimeOutlined, FolderOutlined} from "@mui/icons-material";
+import hand from '../assets/Group 89.svg';
 
 const SingleCourse = () => {   
     return(
         <>
             <div className="container">
                 <div className="row">
-                    <div className="col-12">
+                    <div className={`col-12 ${styles.single_course_container}`}>
                         <div className={styles.course_info}>
                             <div className={styles.right_box}>
                                 <h2 className={styles.title}>دوره طراحی هویت بصری</h2>
@@ -14,10 +16,24 @@ const SingleCourse = () => {
                                 <p className={styles.course_badge}>سطح مقدماتی</p>
                             </div>
                             <div className={styles.left_box}>
-                                <p>مدرس: عطیه جلیلی</p>
-                                <p>در این دوره شما با مفاهیم بنیادین برند آشنا شده و اجزای یک هویت را بصری درک می‌کنید.</p>
+                                <p className={styles.course_teacher}>مدرس: عطیه جلیلی</p>
+                                <p className={styles.course_description}>در این دوره شما با مفاهیم بنیادین برند آشنا شده و اجزای یک هویت را بصری درک می‌کنید.</p>
                                 <div className={styles.icons_group}>
-                                    
+                                    <div>
+                                        <AccessTimeOutlined />
+                                        6 ساعت
+                                    </div>
+                                    <div>
+                                        <FolderOutlined />
+                                        4 جلسه
+                                    </div>
+                                </div>
+                                <div className={styles.course_registration}>
+                                    <div className={styles.text_group}>
+                                        <p>2,000,000 تومان</p>
+                                        <a href="#">ثبت نام دوره</a>
+                                    </div>
+                                    <img src={hand} className={styles.hand_img} alt="Hand" />
                                 </div>
                             </div>
                         </div>
