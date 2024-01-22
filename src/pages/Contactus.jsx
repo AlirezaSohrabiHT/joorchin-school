dbbbc77877b490b9c31b4f33eea8ebd577800ce0
Ashkan Khorasani <ashkankhorasanidevelopment@gmail.com>
2024-01-22 15:16:35 +0330

    ContactUs Component Completed

--- src/pages/Contactus.jsx
+++ src/pages/Contactus.jsx
@@ -1,7 +1,15 @@
-import { Email, Instagram, Telegram, WhatsApp, X } from "@mui/icons-material";
+import { Email, EmailOutlined, Instagram, LinkedIn, Telegram, WhatsApp, X } from "@mui/icons-material";
 import React from "react";
 import background from "../assets/Contact-us.svg";
-import hand from "../assets/click-hand.svg"
+import hand from "../assets/click-hand.svg";
+import whatsapp from '../assets/ic_twotone-whatsapp.svg';
+import telegram from '../assets/la_telegram-plane.svg';
+import mail from '../assets/mail.svg';
+import linkedin from '../assets/linkedin.svg';
+import twitter from '../assets/twitter.svg';
+import instagram from '../assets/instagram (1).svg';
+import listItem from '../assets/Ellipse 2.svg';
+
 import styles from "./Contactus.module.css"
 const Contactus = () => {
 
@@ -10,60 +18,95 @@ const Contactus = () => {
         <div className={`container`}>
             <div className={`row`}>
                 <div className={`col-12 ${styles.container}`}>
-                    <p className={styles.title}>ارتباط با ما</p>
-                    <p></p>
-                    <div className={`${styles.main_container}`}>
-                        <div className={`${styles.contact_us}`}>
-                            <div>
+                    <p className={styles.contact_us_title}>ارتباط با ما</p>
+                    <p className={styles.contact_us_des}>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</p>
+                    <div className={styles.contact_us_box}>
+                        <div className={styles.contact_us_top_box}>
+                            <div className={styles.right_top_box}>
                                 <p>راه های ارتباطی</p>
-                                <Email/>
-                                <Telegram/>
-                                <WhatsApp/>
+                                <div className={styles.icons_group}>
+                                    <a href="#">
+                                        <img src={mail} alt="Mail icon" />
+                                    </a>
+                                    <a href="#">
+                                        <img src={telegram} alt="Telegram icon" />
+                                    </a>
+                                    <a href="#">
+                                        <img src={whatsapp} alt="Whatsapp icon" />
+                                    </a>
+                                </div>
                             </div>
-                            <hr/>
-                            <div>
-                                <p> شبکه های اجتماعی </p>
-                                <Email/>
-                                <Telegram/>
-                                <WhatsApp/>
-                                <X/>
-                                <Instagram/>
+                            <div className={styles.left_top_box}>
+                                <p>شبکه های اجتماعی</p>
+                                <div className={styles.icons_group}>
+                                    <a href="#">
+                                        <img src={instagram} alt="Instagram icon" />
+                                    </a>
+                                    <a href="#">
+                                        <img src={linkedin} alt="LinkedIn icon" />
+                                    </a>
+                                    <a href="#">
+                                        <img src={twitter} alt="Twitter icon" />
+                                    </a>
+                                    <a href="#">
+                                        <img src={telegram} alt="Telegram icon" />
+                                    </a>
+                                </div>
                             </div>
                         </div>
-                        <div className={styles.data_container}>
-                            <div className={styles.number_container}>
+                        <div className={styles.contact_us_bottom_box}>
+                            <div className={styles.bottom_box_right}>
                                 <div>
-                                    <p className={styles.label}>شماره تماس جورچین</p>
-                                    <p>05138012312-09151231231</p>
+                                    <span>
+                                        <img src={listItem} alt="Circle" />
+                                        <p>شماره تماس جورچین</p>
+                                    </span>
+                                    <p>
+                                        <a href="tel:">05138012312</a>
+                                        <br />
+                                        <a href="tel:">09151231231</a>
+                                    </p>
                                 </div>
                                 <div>
-                                    <p className={styles.label}>آدرس دفتر جورچین</p>
-                                    <p>مشهد-هاشمیه،سامانیه ۱۱، پلاک ۱۱</p>
+                                    <span>
+                                        <img src={listItem} alt="Circle" />
+                                        <p>آدرس دفتر جورچین</p>
+                                    </span>
+                                    <p>
+                                        <a href="tel:">مشهد، هاشمیه، سامانیه 11، پلاک 11</a>
+                                    </p>
                                 </div>
                             </div>
-                            <div>
+                            <div className={styles.bottom_box_left}>
                                 <div>
-                                    <p className={styles.label}>فرم ارتباط با ما</p>
-                                    <p>در کنار روش‌های که  برای ارتباط با جورچین در نظر گرفته‌ایم،شما همچنین می‌توانید از طریق فرم زیر  پیام خود را برای ما ارسال کنید.</p>
+                                    <span>
+                                        <img src={listItem} alt="Circle" />
+                                        <p>شماره تماس جورچین</p>
+                                    </span>
+                                    <p>در کنار روش هایی که برای ارتباط با جورچین در نظر گرفته‌ایم، شما همچنان می‌توانید از طریق فرم زیر پیام خود را برای ما ارسال کنید.</p>
                                 </div>
-                                <div>
-                                    <div className={styles.input_container}>
-                                        <label>نام شما:</label>
-                                        <input className={styles.input} type="text" id="name" name="name" required minlength="4" maxlength="8" size="10" />
-                                        <label>متن پیام:</label>
-                                        <input className={styles.input}  type="text" id="name" name="name" required minlength="4" maxlength="8" size="10" />
+                                <div className={styles.form_group}>
+                                    <div className={styles.form_right_group}>
+                                        <div>
+                                            <label htmlFor="">نام شما:</label>
+                                            <br />
+                                            <input type="text" />
+                                        </div>
+                                        <div>
+                                            <label htmlFor="">آدرس ایمیل:</label>
+                                            <br />
+                                            <input type="text" />
+                                        </div>
                                     </div>
-                                    <div className={styles.input_container}>
-                                    <label>آدرس ایمیل:</label>
-                                        <input className={styles.input}  type="email" id="name" name="name" required minlength="4" maxlength="8" size="10" />
+                                    <div>
+                                        <label htmlFor="">متن پیام:</label>
+                                            <br />
+                                            <textarea name="" id="" cols="30" rows="5"></textarea>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div>
-
-                        </div>
-                        <img className={styles.hand} src={hand}/>
+                        <img src={hand} className={styles.hand_img} alt="Hand" />
                     </div>
                 </div>
             </div>
